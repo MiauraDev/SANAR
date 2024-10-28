41cbc5e66303b473c22df0077f4c7a7fb3e4b754
Laura Luque <lauraluque1215@hotmail.com>
2024-10-28 16:13:53 -0500

    fix: :fire: production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,14 +92,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Enable DNS rebinding protection and other `Host` header attacks.
+  # config.hosts = [
+  #   "example.com",     # Allow requests from example.com
+  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+  # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-end
-
-config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-
-if ENV["RAILS_LOG_TO_STDOUT"].present?
-  logger           = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.logger = ActiveSupport::TaggedLogging.new(logger)
 end
