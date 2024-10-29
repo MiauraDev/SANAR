1a92949142a30a9883c43245938d6e71f4bcd412
Laura Luque <lauraluque1215@hotmail.com>
2024-10-29 00:12:27 -0500

    docs: :fire: Detalles

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy", as: :logout
 
-
-
   resources :medicamentos
   resources :tratamientos
   resources :diagnosticos
